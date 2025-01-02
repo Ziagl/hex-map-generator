7b533dafe079392038e2ce47cbf3f41c1c661872
Ziagl <office@ziegelwanger-edv.at>
2025-01-02 09:56:06 +0100

    version 0.1.14

---
+++ HexMapGenerator/models/TileType.cs
@@ -0,0 +1,11 @@
+﻿using com.hexagonsimulations.HexMapGenerator.Enums;
+
+namespace com.hexagonsimulations.HexMapGenerator.Models;
+
+// fully describes single tile of map
+public record TileType
+{
+    public TerrainType terrainType;
+    public LandscapeType landscapeType;
+    public RiverType riverType;
+}
